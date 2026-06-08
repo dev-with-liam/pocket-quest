c831c8591e904c97427d91607c286431aa1e8f2f
Liam Sood <liamsood@gmail.com>
2026-06-08 15:31:05 -0700

    Initial Commit

---
+++ pocket-quest/pocket_questApp.swift
@@ -0,0 +1,32 @@
+//
+//  pocket_questApp.swift
+//  pocket-quest
+//
+//  Created by Liam Sood on 6/8/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pocket_questApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
